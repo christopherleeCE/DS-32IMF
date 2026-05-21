d0a6bd92bf924b0a04a6999d2cdacb5e25b15908
christoherleeCE <christopher.lee.engineer@outlook.com>
2026-05-21 05:21:21 -0700

    N/A

--- Programs/directed/functional_c/junk.c
+++ Programs/directed/functional_c/junk.c
@@ -124,61 +124,83 @@ __attribute__((section(".vram"), used))
 uint16_t frame[] = {FULL_FRAME};
 
 __attribute__((section(".tram"), used))                    
-char text_buffer[368] = {[0 ... 367] = (char)0x9D};
+char text_buffer[368] = {
+  /*"##########################"*/ 
+    "                          "
+    " @@@   @@@    @@@@@@@  @  "
+    " @  @ @      @  __   @  @ "
+    " @  @  @@   @  @@@@ @  @  "
+    " @  @    @   @  ^^    @   "
+    " @@@  @@@     @@@@@@@@    "
+    "                          "
+    " @@@  @@@  @@@ @   @ @@@@ "
+    "    @    @  @  @@ @@ @    "
+    " @@@    @   @  @ @ @ @@@@ "
+    "    @  @    @  @   @ @    "
+    " @@@  @@@@ @@@ @   @ @    "
+    "                          "
+    "                          "
+  /*"##########################"*/ 
+};
 
 int junk[] = {1, 2, 3, 4, 5};
 
 int main(){
 
-    // size_t ret_arr[5];
-    // char tmp[2];
+    size_t ret_arr[5];
+    char tmp[2];
 
-    // wait_fixed();
-    // wait_fixed();
-    // wait_fixed();
-    // wait_fixed();
-    // wait_fixed();
+    wait_fixed();
+    wait_fixed();
+    wait_fixed();
+    wait_fixed();
+    wait_fixed();
+    wait_fixed();
+    wait_fixed();
+    wait_fixed();
+    wait_fixed();
+    wait_fixed();
 
-    // wait_fixed();
-    // newline();
-    // ret_arr[0] = write("hello");
-    // newline();
-    // tmp[0] = (char)ret_arr[0] + 48;
-    // tmp[1] =  '\0';
-    // write(tmp);
+    wait_fixed();
+    newline();
+    ret_arr[0] = write("hello");
+    newline();
+    tmp[0] = (char)ret_arr[0] + 48;
+    tmp[1] =  '\0';
+    write(tmp);
 
-    // wait_fixed();
-    // newline();
-    // ret_arr[1] = write("hi");
-    // newline();
-    // tmp[0] = (char)ret_arr[1] + 48;
-    // write(tmp);
+    wait_fixed();
+    newline();
+    ret_arr[1] = write("hi");
+    newline();
+    tmp[0] = (char)ret_arr[1] + 48;
+    write(tmp);
 
-    // wait_fixed();
-    // newline();
-    // ret_arr[2] = write("penis");
-    // newline();
-    // tmp[0] = (char)ret_arr[2] + 48;
-    // write(tmp);
+    wait_fixed();
+    newline();
+    ret_arr[2] = write("penis");
+    newline();
+    tmp[0] = (char)ret_arr[2] + 48;
+    write(tmp);
 
-    // wait_fixed();
-    // newline();
-    // ret_arr[3] = write("cockatoo");
-    // newline();
-    // tmp[0] = (char)ret_arr[3] + 48;
-    // write(tmp);
+    wait_fixed();
+    newline();
+    ret_arr[3] = write("cockatoo");
+    newline();
+    tmp[0] = (char)ret_arr[3] + 48;
+    write(tmp);
 
-    // wait_fixed();
-    // newline();
-    // ret_arr[4] = write("REDACTED");
-    // newline();
-    // tmp[0] = (char)ret_arr[4] + 48;
-    // write(tmp) + 48;
+    wait_fixed();
+    newline();
+    ret_arr[4] = write("REDACTED");
+    newline();
+    tmp[0] = (char)ret_arr[4] + 48;
+    write(tmp) + 48;
 
-    // wait_fixed();
-    // newline();
-    // write("bcd_test...");
-    // print_uint(0);
+    wait_fixed();
+    newline();
+    write("bcd_test...");
+    print_uint(-1);
 
     return tb_return(0, 0);
 
